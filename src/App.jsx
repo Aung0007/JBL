26ec713bd8af3856b02288089272db565420009c
Aung0007 <aungmarma593@gmail.com>
2024-07-27 22:45:00 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Nav from './Component/Nav'
 import Main from './Component/Main'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import '../src/jav'
+
 
 const App = () => {
   return (
