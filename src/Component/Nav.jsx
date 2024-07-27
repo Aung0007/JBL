86e086ecb93987ccee3873e99da4029298b33006
Aung0007 <aungmarma583@gmail.com>
2024-07-27 22:19:11 +0600

    add usestate

--- src/Component/Nav.jsx
+++ src/Component/Nav.jsx
@@ -1,6 +1,6 @@
 
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '../jav'
 
@@ -40,6 +40,39 @@ const Nav = () => {
       behavior:"smooth"
     })
   }
+
+  useEffect(()=>{
+    
+
+  // show menu
+  const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close')
+
+//Menu show
+if(navToggle){
+navToggle.addEventListener('click', ()=>{
+navMenu.classList.add('show-menu')
+});
+}
+
+//Menu hidden
+if(navClose){
+navClose.addEventListener('click', ()=>{
+navMenu.classList.remove('show-menu')
+})
+}
+
+//remove menu mobile
+const navLink = document.querySelectorAll('.nav-link')
+
+const linkAction = () =>{
+const navMenu = document.getElementById('nav-menu')
+navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+  })
 
   return (
     <>
